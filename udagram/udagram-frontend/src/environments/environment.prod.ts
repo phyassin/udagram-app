331c6aac0f90202de9db15595c6501a044a39853
ahmed yassin <ph.yasso@gmail.com>
2022-10-11 19:18:24 +0200

    fourth commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-app-dev.eba-gkritgsz.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-app-dev.eba-gkritgsz.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
