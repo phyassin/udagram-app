040239c26a35b4ac7c08adac2e0b7b4a20dd219e
ahmed yassin <ph.yasso@gmail.com>
2022-10-11 20:54:21 +0200

    finall commit

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive ./www s3://ahmed-udagram/ --profile default
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://ahmed-udagram/
+# aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://ahmed-udagram/
